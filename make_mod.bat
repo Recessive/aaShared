40196656310f5c38c020a7cf2607d76c1d5b2059
Sean Gribben <AA.mindustry@gmail.com>
2022-11-12 02:11:59 +1100

    Initial commit

---
+++ make_mod.bat
@@ -0,0 +1,3 @@
+echo Making jar
+CALL gradlew jar
+move build\libs\aaShared.jar "C:\Users\t7i6g\Desktop\stuff\mindustry_server\Custom Testing\config\mods\aaShared.jar"
---
+++ src/main/CustomEvents.java
@@ -0,0 +1,39 @@
+package main;
+
+import mindustry.gen.Player;
+
+public class CustomEvents {
+
+    public static class PlayerJoinSecondary{
+        public final Player player;
+        public final int playTime;
+        public final int donatorLevel;
+
+        public PlayerJoinSecondary(Player player, int playTime, int donatorLevel){
+            this.player = player;
+            this.playTime = playTime;
+            this.donatorLevel = donatorLevel;
+        }
+    }
+
+    public static class NewName{
+        public final String uuid;
+
+        public NewName(String uuid) {
+            this.uuid = uuid;
+        }
+    }
+
+    public static class HudToggle{
+        public final String uuid;
+        public final boolean enabled;
+
+        public HudToggle(String uuid, boolean enabled) {
+            this.uuid = uuid;
+            this.enabled = enabled;
+        }
+    }
+
+    public static class GameOver{
+    }
+}
